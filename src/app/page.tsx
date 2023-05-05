bb6e41e77c85315195f45001f2dd5263e5111d97
Prity46 <132493553+Prity46@users.noreply.github.com>
2023-05-06 01:50:20 +0530

    home and about page with responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Home from "./components/Home/Main";
+import About from "./components/Home/About";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Main() {
-  return <Home />;
+  return <About />;
 }
