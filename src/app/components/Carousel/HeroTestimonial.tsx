c92c7f626a02696b0c627934bccb59edf96f3795
Prity46 <132493553+Prity46@users.noreply.github.com>
2023-05-12 01:26:31 +0530

    home slider

--- src/app/components/Carousel/HeroTestimonial.tsx
+++ src/app/components/Carousel/HeroTestimonial.tsx
@@ -10,7 +10,7 @@ export default function HeroTestimonial() {
   return (
     <>
       <Swiper
-        className="mySwiper h-[95%] w-[344px] home_banner_carousel rounded-[20px]"
+        className="mySwiper h-full w-[344px] home_banner_carousel rounded-[20px]"
         spaceBetween={50}
         pagination={{
           clickable: true,
@@ -24,7 +24,7 @@ export default function HeroTestimonial() {
         </SwiperSlide>
         <SwiperSlide>
           <Swiper
-            className="mySwiper2 swiper-v"
+            className="mySwiper2 swiper-v relative"
             direction={"vertical"}
             spaceBetween={50}
             pagination={{
@@ -34,33 +34,39 @@ export default function HeroTestimonial() {
           >
             <SwiperSlide>
               <div className="h-full w-full">
-                <Image src={homebannerSlider} className="h-full w-full object-cover" alt="..." />
+                <Image src={homebannerSlider} className="w-full object-cover" alt="..."  />
+              </div>
+            </SwiperSlide>
+            {/* <SwiperSlide>
+              <div className="h-full w-full">
+                <Image src={homebannerSlider} className="w-full object-cover" alt="..." />
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="h-full w-full">
-                <Image src={homebannerSlider} className="h-full w-full object-cover" alt="..." />
+                <Image src={homebannerSlider} className="w-full object-cover" alt="..." />
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="h-full w-full">
-                <Image src={homebannerSlider} className="h-full w-full object-cover" alt="..." />
+                <Image src={homebannerSlider} className="w-full object-cover" alt="..." />
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="h-full w-full">
-                <Image src={homebannerSlider} className="h-full w-full object-cover" alt="..." />
+                <Image src={homebannerSlider} className="w-full object-cover " alt="..." />
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="h-full w-full">
-                <Image src={homebannerSlider} className="h-full w-full object-cover " alt="..." />
-              </div>
-            </SwiperSlide>
+            </SwiperSlide> */}
           </Swiper>
         </SwiperSlide>
         <SwiperSlide>Horizontal Slide 3</SwiperSlide>
         <SwiperSlide>Horizontal Slide 4</SwiperSlide>
+        <div className="h-16 w-16 bg-[#FF682C] absolute bottom-20 right-6 z-[3] rounded-full flex items-center justify-center">
+          <svg width="17" height="25" viewBox="0 0 17 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect y="0.614258" width="6" height="24" rx="2" fill="white"/>
+          <rect x="11" y="0.614258" width="6" height="24" rx="2" fill="white"/>
+          </svg>
+        </div>
       </Swiper>
     </>
   );
