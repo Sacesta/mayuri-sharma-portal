c926175c4829d44b719fc00755871171d436af92
Prity46 <132493553+Prity46@users.noreply.github.com>
2023-05-14 01:01:53 +0530

    program page and program details page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import { Inter } from "next/font/google";
 import Home from "./components/Home/Main";
 import About from "./components/Home/About";
 import Podcast from "./components/Home/Podcast";
+import Programs from "./components/Home/Programs";
+import ProgramDetails from "./components/Home/ProgramDetails";
 
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Main() {
-  return <Home />;
+  return <ProgramDetails />;
 }
