18396861065403376ed65bd8c16307d1e0196ae9
Madhak Uday <87520841+madhakuday@users.noreply.github.com>
2023-07-22 16:47:57 +0530

    Delete build directory

--- build/router/message.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import * as controller from '../controller/message.js';
-const router = Router();
-router.route('/').post(controller.newMessage);
-router.route('/:id').get(controller.getMessage);
-export default router;
-//# sourceMappingURL=message.js.map
