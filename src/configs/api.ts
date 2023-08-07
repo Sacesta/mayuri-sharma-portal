461b4635c3aceb463e6f1d04a011288ac2a70629
Madhak Uday <87520841+madhakuday@users.noreply.github.com>
2023-08-07 18:23:24 +0530

    Update api.ts

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 
 // Axios instance
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://mayuri-admin-be.onrender.com",
   timeout: 600000,
   headers: {
     "Content-Type": "application/json; charset=utf-8",
