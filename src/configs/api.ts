e0dc187d4b65c88d2ad15b09c848bb320ea7a3b7
aditya singh rawat <80266913+unreal0901@users.noreply.github.com>
2023-08-08 17:31:11 +0530

    Removed the window ref

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const Window = window as any;
-let document = Window.document;
+// const Window = window as any;
+// let document = Window.document;
 
 // Axios instance
 const axiosInstance = axios.create({
@@ -21,38 +21,38 @@ const axiosInstance = axios.create({
 //   return localStorage.getItem("userRefreshToken");
 // }
 
-function showLoading(loadingOverlay: any) {
-  if (loadingOverlay) {
-    document.activeElement.blur();
-    if (loadingOverlay.classList.contains("loaderhidden")) {
-      loadingOverlay.classList.remove("loaderhidden");
-    }
-  }
-}
+// function showLoading(loadingOverlay: any) {
+//   if (loadingOverlay) {
+//     document.activeElement.blur();
+//     if (loadingOverlay.classList.contains("loaderhidden")) {
+//       loadingOverlay.classList.remove("loaderhidden");
+//     }
+//   }
+// }
 
-function disableLoading(loadingOverlay: any) {
-  if (loadingOverlay) loadingOverlay.classList.add("loaderhidden");
-}
+// function disableLoading(loadingOverlay: any) {
+//   if (loadingOverlay) loadingOverlay.classList.add("loaderhidden");
+// }
 
 axiosInstance.interceptors.request.use(
   function (config: any) {
     console.log("Request started");
-    let loadingOverlay = document.querySelector(".loading");
-    showLoading(loadingOverlay);
+    // let loadingOverlay = document.querySelector(".loading");
+    // showLoading(loadingOverlay);
     return config;
   },
   function (error) {
     console.log("Request ended");
-    let loadingOverlay = document.querySelector(".loading");
-    disableLoading(loadingOverlay);
+    // let loadingOverlay = document.querySelector(".loading");
+    // disableLoading(loadingOverlay);
   }
 );
 
 axiosInstance.interceptors.response.use(function (response) {
   console.log(response);
   console.log("Request ended");
-  let loadingOverlay = document.querySelector(".loading");
-  disableLoading(loadingOverlay);
+  // let loadingOverlay = document.querySelector(".loading");
+  // disableLoading(loadingOverlay);
   return response;
 });
 
